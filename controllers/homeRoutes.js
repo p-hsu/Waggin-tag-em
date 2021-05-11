2babbde49822a062ed61852aa8a0a70ee919db17
Felicia Wootton <felicia.wootton@gmail.com>
2021-05-10 21:14:01 -0500

    added homeRoutes and handlebars

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,34 @@
+const sequelize = require('../config/connection');
+const { Pet, User } = require('../models');
+const router = require('express').Router();
+// const withAuth = require('../utils/auth');
+
+router.get('/', (req, res) => {
+    Pet.findAll({
+        attributes: [
+            'name',
+            'about_me',
+            'temperament',
+            'about_you',
+            'age',
+            'sex',
+            'breed'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['user_name']
+            }
+        ]
+    })
+    .then(petData => {
+        const pets = petData.map(pet => pet.get({ plain: true }));
+        res.render('homepage', { pets, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;

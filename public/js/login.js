0b958d98a28fc92e2fd9e671365ba0e0354f5a89
Stacy Martin <sbrown1031@gmail.com>
2021-05-12 19:21:44 -0600

    PR 2

---
+++ public/js/login.js
@@ -0,0 +1,49 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+    // Collect values from the login form
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+    if (email && password) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+        // If successful, redirect the browser to the homepage/dashboard
+        document.location.replace('/homepage');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  const signupFormHandler = async (event) => {
+    event.preventDefault();
+    const name = document.querySelector('#username').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+    if (name && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ name, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+        // If successful, redirect the browser to the homepage/dashboard
+        document.location.replace('/homepage');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+  
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
+  

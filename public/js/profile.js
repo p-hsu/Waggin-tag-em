553db12cf650ee4398488773e5e619e5b89110ba
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-05-14 20:22:07 -0500

    Delete pet functions > removed modal

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,18 +2,27 @@
 const newPupProfileHandler = async (event) => {
     event.preventDefault();
     console.log('function called')
-    const name = document.getElementById('name').value;
-    const human = document.getElementById('human').value;
-    const age = document.getElementById('age').value;
-    const sex = document.getElementById('sex').value;
-    const breed = document.getElementById('breed').value;
-    const temperament = document.getElementById('temperament').value;
-    const about_me= document.getElementById('about_me').value;
-    const about_you = document.getElementById('about_you').value;
+    const name = document.getElementById('new-name').value;
+    const human = document.getElementById('new-human').value;
+    const age = document.getElementById('new-age').value;
+    const sex = document.getElementById('new-sex').value;
+    const breed = document.getElementById('new-breed').value;
+    const temperament = document.getElementById('new-temperament').value;
+    const about_me= document.getElementById('new-about_me').value;
+    const about_you = document.getElementById('new-about_you').value;
     if (name && human && age && sex && breed && temperament && about_me && about_you) {
       const response = await fetch(`/api/pets`, {
         method: 'POST',
-        body: JSON.stringify({ name, human, age, sex, breed, temperament, about_me, about_you }),
+        body: JSON.stringify({
+          name,
+          human,
+          age,
+          sex,
+          breed,
+          temperament,
+          about_me,
+          about_you
+        }),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -57,9 +66,7 @@ const newPupProfileHandler = async (event) => {
   //   }
   // };
   
-  document
-    .querySelector('.new-pet-form')
-    .addEventListener('submit', newPupProfileHandler);
+document.querySelector('.new-pet-form').addEventListener('submit', newPupProfileHandler);
   
 //   document
 //     .querySelector('.post-list')
@@ -68,3 +75,76 @@ const newPupProfileHandler = async (event) => {
   // document
   //   .querySelector('.post-list')
   //   .addEventListener('click', delButtonHandler);
+
+
+
+// EVENT DELEGATION FOR EACH PROFILE CARD
+const petHandler = (event) => {
+  // event delegation to determine delete vs edit btn
+  if (event.target.hasAttribute('data-delete-id')) {
+      deletePet(event);       
+  } else if (event.target.hasAttribute('data-update-id')) {
+      updatePet(event);
+  }
+};
+// get pet.id to DELETE request by id
+const deletePet = async (event) => {
+    const id = event.target.getAttribute('data-delete-id');
+    console.log(id);
+
+    let confirmation = confirm('Are you sure you want to delete this pet profile?');
+
+    if (confirmation === true) {
+        const response = await fetch(`/api/pets/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+    
+// get post.id to go to edit-post view
+const updatePet = async (event) => {
+  event.preventDefault();
+  const id = event.target.getAttribute('data-update-id');
+
+  const name = document.getElementById('update-name').value;
+  const human = document.getElementById('update-human').value;
+  const age = document.getElementById('update-age').value;
+  const sex = document.getElementById('update-sex').value;
+  const breed = document.getElementById('update-breed').value;
+  const temperament = document.getElementById('update-temperament').value;
+  const about_me= document.getElementById('update-about_me').value;
+  const about_you = document.getElementById('update-about_you').value;
+
+  const response = await fetch(`/api/pets/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      name,
+      human,
+      age,
+      sex,
+      breed,
+      temperament,
+      about_me,
+      about_you
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  console.log(response)
+  
+  if (response.ok) {
+    document.location.replace('/profile');
+  } else {
+    alert(`Uh oh, couldn't edit your pet. Try again!`);
+  }
+
+}
+
+document.querySelector('.pet-profile-card').addEventListener('click', petHandler)
